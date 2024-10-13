054115835756cf8546f1f88353ac23d8f14638af
Sowparna Shome <soupornoshome@gmail.com>
2024-10-13 18:32:47 +0530

    mongodb connection

--- todo-server/server.js
+++ todo-server/server.js
@@ -5,3 +5,11 @@ const todoSchema = new mongoose.Schema({
     completed: { type: Boolean, default: false }
 }, { timestamps: true });
 const Todo = mongoose.model("Todo", todoSchema);
+
+try {
+        await mongoose.connect(process.env.MONGODB_URI)
+        .then(() => console.log("MongoDB connected!!!"))
+        .catch(error=>console.log("Error connecting the MongoDB!!! ",error));
+} catch (error) {
+    console.log("MongoDB connection error!!! ",error)
+}
