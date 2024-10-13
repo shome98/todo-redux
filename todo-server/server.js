809ab311447b304a7849f892d7894e57a347bdb5
Sowparna Shome <soupornoshome@gmail.com>
2024-10-13 18:32:00 +0530

    mongoose model create

--- todo-server/server.js
+++ todo-server/server.js
@@ -1,5 +1,7 @@
-import http from "http";
-const server = http.createServer(async (req, res) => {
-    res.end(JSON.stringify({ message: "new server test" }));
-});
-server.listen(6789, () => console.log(`server running on 6789`));
+import mongoose from "mongoose";
+
+const todoSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    completed: { type: Boolean, default: false }
+}, { timestamps: true });
+const Todo = mongoose.model("Todo", todoSchema);
